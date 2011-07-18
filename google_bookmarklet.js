e594cf113eb60dc7b85b86fc027394102d6f415f
Maksim Ryzhikov <rv.maksim@gmail.com>
2011-07-18 15:48:52 -0400

    up google set

---
+++ google_bookmarklet.js
@@ -0,0 +1,48 @@
+/* AUTHOR:  Maksim Ryzhikov
+ * NAME:    BUZZ
+ * VERSION: 0.1
+ */
+
+group.commands.add(["gbuzz", "buzz"], "Google BUZZ", function (args) {
+	var w = XPCNativeWrapper.unwrap(window.content.window);
+	with (w) {
+		eval("q=location.href;if(document.getSelection){d=document.getSelection();}else{d='';};p=document.title;void(open('http://www.google.com/reader/link?url='+encodeURIComponent(q)+'&title='+encodeURIComponent(p)+'&srcURL=&srcTitle=via%20here&snippet='+encodeURIComponent(d),'Google%20Buzz','toolbar=no,width=700,height=350'));");
+	}
+},
+{
+	argCount: "0"
+});
+
+/* AUTHOR: Maksim Ryzhikov
+ * NAME: GOOGLE-READER NOTE
+ * VERSION: 0.1
+ */
+
+group.commands.add(["gnote", "gnt"], "Create Link Bookmarklet in Google Reader", function (args) {
+	var w = XPCNativeWrapper.unwrap(window.content.window);
+	with (w) {
+		    eval("var b = document.body; var GR________bookmarklet_domain = 'https://www.google.com'; if (b && !document.xmlVersion) { void(z = document.createElement('script')); void(z.src = 'https://www.google.com/reader/ui/link-bookmarklet.js'); void(b.appendChild(z)); } else {}");
+	}
+},
+{
+	argCount: "0"
+});
+
+/* AUTHOR: Maksim Ryzhikov
+ * AUTHOR BOOKMARKLET AMIT AGARWAL
+ * URL http://www.labnol.org/internet/google-plus-one-bookmarklet/19474/
+ * NAME: GOOGLE-PLUS-ONE
+ * VERSION: 0.1
+ * NOTE: THIS IS NOT OFFICIAL BOOKMARKLET
+ */
+
+
+group.commands.add(["gplus", "gx"], "The Google Plus One Bookmarklet", function (args) {
+	var w = XPCNativeWrapper.unwrap(window.content.window);
+	with (w) {
+		eval('(function(){var c;c=document.createElement("script");c.type="text/javascript";c.src="http://www.ctrlq.org/plusone/index.js?r="+Math.random();document.body.appendChild(c);})();');
+	}
+},
+{
+	argCount: "0"
+});
